7572417f6c60ea3e5c2ae6d600c6f7692d1f589c
Jeremy Xue <jer1506xue@gmail.com>
2024-11-22 11:41:32 -0600

    Add RESP tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,4 @@
 #include "resp.h"
 
 int main(int argc, char **argv) {
-    SimpleString simple_str("Hello World!");
-    std::cout << simple_str.serialize();
-    Error error("An error occurred...");
-    std::cout << error.serialize();
-    BulkString bulk_str("Bulk string!");
-    std::cout << bulk_str.serialize();
-    Integer x(22);
-    std::cout << x.serialize();
-    std::vector<std::unique_ptr<RType>> nested;
-    nested.push_back(std::make_unique<Integer>(1));
-    nested.push_back(std::make_unique<Integer>(2));
-    std::unique_ptr<Array> nested_arr = std::make_unique<Array>(nested);
-
-    std::vector<std::unique_ptr<RType>> vec;  
-    vec.push_back(std::make_unique<SimpleString>("Element 0"));
-    vec.push_back(std::move(nested_arr));
-    Array arr(vec);
-
-    std::cout << arr.serialize();
 }
--- tests/resp.test.cpp
+++ tests/resp.test.cpp
@@ -2,13 +2,38 @@
 #include <catch2/catch_all.hpp>
 #include "../src/resp.h"
 
-// sanity check... 
 TEST_CASE("Math works!") {
     REQUIRE(2+2 == 4); 
 }
 
-TEST_CASE("Serialize simple string") {
+TEST_CASE("Serialize Simple String", "[RESP]") {
     SimpleString simple_str("Hello World!");
-    std::string serialized = simple_str.serialize();
-    REQUIRE(serialized == "+Hello World!\r\n");
+    REQUIRE(simple_str.serialize() == "+Hello World!\r\n");
+}
+
+TEST_CASE("Serialize Error", "[RESP]") {
+    Error error("An error occurred...");
+    REQUIRE(error.serialize() == "-An error occurred...\r\n");
+}
+
+TEST_CASE("Serialize Bulk String", "[RESP]") {
+    BulkString bulk_str("This is a bulk string!");
+    REQUIRE(bulk_str.serialize() == "$22\r\nThis is a bulk string!\r\n");
+}
+
+TEST_CASE("Serialize Integer", "[RESP]") {
+    Integer x(22);
+    REQUIRE(x.serialize() == ":22\r\n");
+}
+
+TEST_CASE("Serialize Array", "[RESP]") {
+    std::vector<std::unique_ptr<RType>> nested;
+    nested.push_back(std::make_unique<Integer>(1));
+    nested.push_back(std::make_unique<Integer>(2));
+    std::unique_ptr<Array> nested_arr = std::make_unique<Array>(nested);
+    std::vector<std::unique_ptr<RType>> vec;  
+    vec.push_back(std::make_unique<SimpleString>("Element 0"));
+    vec.push_back(std::move(nested_arr));
+    Array arr(vec);
+    REQUIRE(arr.serialize() == "*2\r\n+Element 0\r\n*2\r\n:1\r\n:2\r\n");
 }
